536cf125f65d1ecd3673a5f4a8a6123e4cf5b41b
jonifelsmann <116090903+jonifelsmann@users.noreply.github.com>
2022-11-05 14:14:30 +0100

    bildergalier, muss noch angepasst werden

---
+++ season_overview.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <meta charset="UTF-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<meta name="description" content="Sei ein Teil von dem gr��ten Bierballzusammenschlu� in ganz Deutschland." />
+		<meta name="keywords" content="Bierball, Flunkyball, Turnier, Dresden, Berlin, Bier, Sterni, Sternburg" />
+		<meta name="author" content="Jonathan Felsmann & Maximilian Rentzsch" />
+		<meta name="copyright" content="Jonathan Felsmann & Maximilian Rentzsch" />
+		<meta name="publisher" content="N.B.B.L - Team" />
+		<title>Bierball - Turniere</title>
+        <lin href="https://blankiball.de/images/hermann_logo/export.png="shortcut icon"  type="image/x-icon" />
+        <link href="https://blankiball.de/images/hermann_logo/export.png" rel="icon" type="image/x-icon" />
+
+
+        <link href="season_overview.css" rel="stylesheet" type="text/css" media="screen" />
+
+</head>
+
+
+<body background="bilder\bier.jpg">
+
+        <div id="head_container"> 
+			<div id="header"> <span class="Stil1">&raquo;</span> N.B.B.L - Seasonübersicht <span class="Stil1">&laquo;</span>
+			</div>
+			<!-- ende header -->
+
+<div id="nbbl_logo">
+	<a href="impressum.php"><img src="bilder\wgwa-schwebend-logo.png" height="320" center"></a>
+	<a href="impressum.php"><img src="bilder\nbbl-logo.png" height="300"></a>
+	<a href="impressum.php"><img src="bilder\ringbahn.jpg" height="250"></a>
+</div>
+
+
+<!--nav bar -->
+
+<div id= "nav_bar">
+
+<a href="start.php">Startseite</a>
+<a href="">Seasonübersicht</a>
+<a href="season_overview.php">Seasonübersicht</a>
+<a href= "team_registration.php">Teamanmeldung</a>
+<a href= "impressum.php">Impressum</a>
+
+
+</div>
+
+
+<!-- ende nav_bar -->
+   
+<!-- ende head container -->	
+
+
+            <!-- begin main container -->
+
+		<div id="main_container">
+
+            <div id="left_sidebar_info">
+            
+
+            </div>
+
+			<div id="main"><!-- begin main -->
+
+
+			<div class="gallery">
+  <a target="_blank" href="bilder\team1.png">
+    <img src="bilder\team1.png" alt="Cinque Terre" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="bilder\team1.png">
+    <img src="bilder\team1.png" alt="Forest" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="bilder\team1.png">
+    <img src="bilder\team1.png" alt="Northern Lights" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="bilder\team1.pngg">
+    <img src="bilder\team1.png" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Add a descrdddddddiption of the image here</div>
+</div>
+        
+
+            </div><!-- ende main -->
+
+
+</div> <!-- ende main container -->
+
+        <!-- Begin Impressum -->
+
+        
+         <div id="footer_container">
+             <div id="footer">Alle hier auf der Webseite entstandenden Fehler weisen wir komplett zurück. </br>
+			(C) 2022 REFELS GmbH
+
+
+	        </div><!-- ende footer -->
+	    </div><!-- ende footer container -->
+
+
+    </body>
+</html>
+
