03bebfc398f037b2ad40c504bcac2779482e374b
jonifelsmann <116090903+jonifelsmann@users.noreply.github.com>
2022-11-06 14:10:17 +0100

    videoeinbindung

--- start.php
+++ start.php
@@ -92,7 +92,7 @@ echo $sec." Sec ";
 <!-- ende nav_bar -->
 
 <p> <video width="320" height="240" controls>
-<source src="IMG_0525.mov" type="video/mov">
+<source src="IMG_0525.MOV" type="video/MOV">
 Video vom bierballturneir
 </video>
 
