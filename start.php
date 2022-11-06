a0e4321bb34502f94364b5c67a4ad0eeca03e862
maxcito <max.rentzsch@outlook.com>
2022-11-06 23:43:18 +0100

    Titel und css link aktualisiert

--- start.php
+++ start.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NBBL Impressum</title>
+    <title>NBBL Startseite</title>
 
     <meta charset="UTF-8">
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
@@ -18,7 +18,7 @@
         <link href="https://blankiball.de/images/hermann_logo/export.png" rel="icon" type="image/x-icon" />
 
 
-        <link href="impressum_style.css" rel="stylesheet" type="text/css" media="screen" />
+        <link href="start.css" rel="stylesheet" type="text/css" media="screen" />
 
 </head>
 										
