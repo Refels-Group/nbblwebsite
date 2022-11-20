dfe04e174084ab21b2d651845eddec34b86a5833
Maximilian Rentzsch <max.rentzsch@outlook.com>
2022-11-20 19:29:34 +0100

    add login page

---
+++ .php_tmp/loginpage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <head>
+		<meta charset="UTF-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<meta name="description" content="Sei ein Teil von dem gr��ten Bierballzusammenschlu� in ganz Deutschland." />
+		<meta name="keywords" content="Bierball, Flunkyball, Turnier, Dresden, Berlin, Bier, Sterni, Sternburg" />
+		<meta name="author" content="Jonathan Felsmann" />
+		<meta name="copyright" content="Jonathan Felsmann" />
+		<meta name="publisher" content="N.B.B.L - Team" />
+		<title>Bierball - Turniere</title>
+        <lin href="https://blankiball.de/images/hermann_logo/export.png="shortcut icon"  type="image/x-icon" />
+        <link href="https://blankiball.de/images/hermann_logo/export.png" rel="icon" type="image/x-icon" />
+
+		<link rel="icon" type="image/png" href="bilder\nbbl-logo">
+		<link href="login_page.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+    
+</body>
+</html>
