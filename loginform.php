aefdce51bbcdc88fa2a4f8661b7b9b0bae709153
maxcito <max.rentzsch@outlook.com>
2023-01-23 21:20:57 +0100

    Test

--- loginform.php
+++ loginform.php
@@ -2,50 +2,29 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-<link href="loginform.css" rel="stylesheet" type="text/css" media="screen"/> 
-
+    <title>Login Form</title>
+    <link href="loginform.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-
-
-    
-<div id="main">
-
-<div>
-    <?php
-  
-    ?>
-</div>
-
-
-
-    <form id="form" action="index_datatest.php" method="post">
-        
-    <table>
-
-    <tr>
-    <td id="regbutton">Anmelden</td>
-    <br>
-  <tr>
-    <td>Email</td>
-    <td><input id="txtf" type="text" name="EMail"></td>
-  </tr>
-  <tr>
-    <td>Password</td>
-    <td><input id="txtf" type="text" name="Password"></td>    
-  </tr>
-
-</table> 
-<button id="sbutton" name="name" value="value" type="submit">Anmelden</button>
-
-    </form>
-    <button id="sbutton" href="form.php">Registrieren</button>
-
-</div>
-
+    <div id="main">
+        <form id="form" action="index_datatest.php" method="post">
+            <table>
+                <tr>
+                    <td id="regbutton">Anmelden</td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td><input id="txtf" type="email" name="EMail"></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input id="txtf" type="password" name="Password"></td>    
+                </tr>
+            </table> 
+            <button id="sbutton" type="submit">Anmelden</button>
+        </form>
+        <a href="form.php"><button id="sbutton">Registrieren</button></a>
+    </div>
 </body>
 </html>
